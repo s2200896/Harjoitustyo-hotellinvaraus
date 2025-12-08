a2be022458b5cedcca55e985be8ca3f96cf1862f
s2200896 <sisu.salakoski@gmail.com>
2025-12-09 00:08:59 +0200

    Update ValidateName(): reject empty name

--- Harjoitustyo-hotelli/ValidateName.cpp
+++ Harjoitustyo-hotelli/ValidateName.cpp
@@ -5,6 +5,10 @@ bool ValidateName(const std::string& s) {
 	// Locale for recognizing UTF-8 letters
 	std::locale loc("fi_FI.utf8");
 
+	if (s.empty()) {
+		return false;
+	}
+
 	for (const char c : s) {
 		if (c == ';') {
 			return false;
